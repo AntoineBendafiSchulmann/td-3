40696ec4dcfc747fbf8a5b2c8365cf56cc364d4a
abendafi <bendafischulmann@gmail.com>
2023-07-03 15:00:54 +0200

    init project

---
+++ tests/ApiTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require 'Api.php';
+
+class AnimeCharacterFetcherTest extends TestCase
+{
+    private $fetcher;
+
+    protected function setUp(): void
+    {
+        $this->fetcher = new AnimeCharacterFetcher();
+    }
+
+    public function testFetchCharacterNamesAndLinks()
+    {
+        $namesAndLinks = $this->fetcher->fetchCharacterNamesAndLinks();
+
+        // Make sure we got an array
+        $this->assertIsArray($namesAndLinks);
+
+        // Make sure the array isn't empty
+        $this->assertNotEmpty($namesAndLinks);
+
+        // Check a few items in the array to make sure they're formatted correctly
+        foreach ($namesAndLinks as $item) {
+            $this->assertIsArray($item);
+            $this->assertArrayHasKey('name', $item);
+            $this->assertArrayHasKey('link', $item);
+        }
+    }
+}
