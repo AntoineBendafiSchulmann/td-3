765652078e86de5859c49696b108087071c53043
abendafi <bendafischulmann@gmail.com>
2023-07-03 15:10:19 +0200

    init project 2

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# td3
+# IIM TD2
+
+[![Last version](https://img.shields.io/packagist/v/antoinebendafi/iim-td3?maxAge=3600)](https://packagist.org/packages/antoinebendafi/iim-td3)
+
+## Installation
+
+```bash
+composer require AntoineBendafiSchulmann/td3
+```
+
+## Local development
+
+```bash
+composer install
+```
+
+```bash
+ php vendor/bin/phpstan analyse src --level=max
+```
+
+
+```bash
+php vendor/bin/php-cs-fixer fix src --rules=@PSR12
+```
+
+```bash
+php vendor/bin/phpunit tests
+```
---
+++ src/Api.php
@@ -0,0 +1,39 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class AnimeCharacterFetcher
+{
+    private $client;
+    private $crawler;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->crawler = new Crawler();
+    }
+
+    public function fetchCharacterNamesAndLinks()
+    {
+        $response = $this->client->get("https://www.nautiljon.com/animes/naruto+shippuden/personnages.html");
+        $this->crawler->addHtmlContent($response->getBody()->getContents());
+
+        $characterNamesAndLinks = $this->crawler->filter('a.tooltip')->each(function (Crawler $node) {
+            return [
+                'name' => $node->attr('title'),
+                'link' => "https://www.nautiljon.com" . $node->attr('href'),
+            ];
+        });
+
+        return $characterNamesAndLinks;
+    }
+}
+
+// Example usage:
+$fetcher = new AnimeCharacterFetcher();
+
+$namesAndLinks = $fetcher->fetchCharacterNamesAndLinks();
+print_r($namesAndLinks);
